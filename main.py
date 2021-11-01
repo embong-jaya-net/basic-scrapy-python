edcbfba9f7b88579426f07b329c0ba690c8f5fb4
togarmedan <jauhari.andi@gmail.com>
2021-11-01 21:30:52 +0700

    modularisasi dengan fungsi

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Modularisasi dengan function
+"""
+
+
+def ekstraksi_data():
+    hasil = dict()
+    hasil['tanggal'] = '24 Agustus 2021'
+    hasil['waktu'] = '12:05:52 WIB'
+    hasil['magnitudo'] = 4.1
+
+    return hasil
+
+
+def tampilkan_data(result):
+    print("Gempa Terakhir Berdasarkan BMKG")
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+
+
+if __name__ == '__main__':
+    print('Aplikasi Utama')
+    data_gempa = ekstraksi_data()
+    tampilkan_data(data_gempa)
+
+
+
+    
+
+
