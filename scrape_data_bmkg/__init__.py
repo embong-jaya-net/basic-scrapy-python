27de230915a61c4a4f01846d4e971b3fe12c60f8
togarmedan <jauhari.andi@gmail.com>
2021-11-02 22:25:02 +0700

    initial commit

--- scrape_data_bmkg/__init__.py
+++ scrape_data_bmkg/__init__.py
@@ -1,17 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
 
 
-def ekstraksi_data():
-    hasil = dict()
-    hasil['tanggal'] = '24 Agustus 2021'
-    hasil['waktu'] = '12:05:52 WIB'
-    hasil['magnitudo'] = 4.1
-    hasil['kedalaman'] = '70 KM'
+def ekstraksi_data_bmkg():
 
-    return hasil
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        scrap = soup.find('span', {'class': 'waktu'})
+        scrap = scrap.text.split(',')
+        tanggal = scrap[0]
+        waktu = scrap[1]
+
+        scrap = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        scrap = scrap.findChildren('li')
+        magnitudo = None
+        kedalaman = None
+        i = 0
+        for data in scrap:
+            # print(i, data)
+            if i == 1:
+                magnitudo = data.text
+            elif i == 2:
+                kedalaman = data.text
+            i = i + 1
+
+        hasil = dict()
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+
+        return hasil
 
 
 def tampilkan_data(result):
-    print("Gempa Terakhir Berdasarkan BMKG")
+    print('Gempa Terakhir Berdasarkan BMKG')
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
     print(f"Magnitudo {result['magnitudo']}")
