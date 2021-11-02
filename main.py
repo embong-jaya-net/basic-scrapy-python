e593aa6abcc6a65b2b2954575fc7eedc71df3cdf
togarmedan <jauhari.andi@gmail.com>
2021-11-02 18:37:49 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 """
 Modularisasi dengan function
 """
-
-
-def ekstraksi_data():
-    hasil = dict()
-    hasil['tanggal'] = '24 Agustus 2021'
-    hasil['waktu'] = '12:05:52 WIB'
-    hasil['magnitudo'] = 4.1
-    hasil['kedalaman'] = '70 KM'
-
-    return hasil
-
-
-def tampilkan_data(result):
-    print("Gempa Terakhir Berdasarkan BMKG")
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Kedalaman {result['kedalaman']}")
-
+from scrape_data_bmkg import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('Aplikasi Utama')
