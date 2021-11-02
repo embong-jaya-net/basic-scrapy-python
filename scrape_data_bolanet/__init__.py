c48f98f3372e90c7076aeb2513e8ad9683aaef66
togarmedan <jauhari.andi@gmail.com>
2021-11-02 22:43:56 +0700

    initial commit

--- scrape_data_bolanet/__init__.py
+++ scrape_data_bolanet/__init__.py
@@ -11,11 +11,12 @@ def ekstraksi_data_bolanet():
     if content.status_code == 200:
 
         soup = BeautifulSoup(content.text, 'html.parser')
-        # scrap = soup.find('div', {'class': 'item'})
+        scrap = soup.find('div', {'class': 'item'})
         # scrap = scrap.findChildren('div', {'class': 'text'})
         # scrap = scrap.findChildren('a', {'class': 'ntitle'})
         # scrap = scrap.text
-        print(soup.prettify())
+        print(scrap.prettify())
+
 
 
 
