1ec945bbb28fa48326b95be62622a94f3ea566ff
togarmedan <jauhari.andi@gmail.com>
2021-11-02 22:24:19 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 """
 Modularisasi dengan function
 """
-from scrape_data_bmkg import ekstraksi_data, tampilkan_data
+from scrape_data_bmkg import ekstraksi_data_bmkg, tampilkan_data
+from scrape_data_bolanet import ekstraksi_data_bolanet
 
 if __name__ == '__main__':
-    print('Aplikasi Utama')
-    data_gempa = ekstraksi_data()
+    print('Aplikasi Utama - Scraping Data from Website')
+    print('-' * 50)
+    print('\n')
+    print('1. Srcapping Data Gempa Terkini dari website BMKG')
+    data_gempa = ekstraksi_data_bmkg()
     tampilkan_data(data_gempa)
-    
 
+    print('\n2. Scraping Berita Liga Champions Terbaru dari Website Bolanet')
+    ekstraksi_data_bolanet()
 
 
     
