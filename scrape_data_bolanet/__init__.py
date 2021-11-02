80ac7a36381def5b7750c49a9fb5d25e9a0eba0d
togarmedan <jauhari.andi@gmail.com>
2021-11-02 22:24:43 +0700

    scrape data bolanet

---
+++ scrape_data_bolanet/__init__.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def ekstraksi_data_bolanet():
+    try:
+        content = requests.get('https://www.bola.net/champions/')
+    except Exception:
+        return None
+
+    if content.status_code == 200:
+
+        soup = BeautifulSoup(content.text, 'html.parser')
+        # scrap = soup.find('div', {'class': 'item'})
+        # scrap = scrap.findChildren('div', {'class': 'text'})
+        # scrap = scrap.findChildren('a', {'class': 'ntitle'})
+        # scrap = scrap.text
+        print(soup.prettify())
+
+
+ekstraksi_data_bolanet()
+
+
+
+
