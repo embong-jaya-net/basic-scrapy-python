7eb614052e919d6a779e86bee617259e7da11305
togarmedan <jauhari.andi@gmail.com>
2021-11-03 15:48:11 +0700

    initial commit

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ from scrape_data_bmkg import ekstraksi_data_bmkg, tampilkan_data
 from scrape_data_bolanet import ekstraksi_data_bolanet
 
 if __name__ == '__main__':
-    print('Aplikasi Utama - Scraping Data from Website')
+    print('\nAplikasi Utama - Scraping Data from Website')
     print('-' * 50)
-    print('\n')
+    print('Pilih Menu :')
     print('1. Srcapping Data Gempa Terkini dari website BMKG')
-    data_gempa = ekstraksi_data_bmkg()
-    tampilkan_data(data_gempa)
+    print('2. Scrapping Data Berita Liga Champions Terbaru dari Website Bolanet')
+    print('3. Coming Soon')
+    menu = input('Pilih salah satu menu:')
+    if menu == '1':
+        print('\n')
+        data_gempa = ekstraksi_data_bmkg()
+        tampilkan_data(data_gempa)
+    elif menu == '2':
+        print('\n2. Scraping Berita Liga Champions Terbaru dari Website Bolanet')
+        ekstraksi_data_bolanet()
+    elif menu == '3':
+        print('\nComing Soon')
+    else:
+        print('\nTidak ada di dalam menu')
 
-    print('\n2. Scraping Berita Liga Champions Terbaru dari Website Bolanet')
-    ekstraksi_data_bolanet()
+
 
 
     
